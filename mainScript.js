3be00c90f40bfd06c275aee15c1b20ce763632b6
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-10-06 19:08:45 +0100

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -1,1 +1,9 @@
-
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       document.getElementById("lessonArea").innerHTML = xhttp.responseText;
+    }
+};
+xhttp.open("GET", "testText.txt", true);
+xhttp.send();
