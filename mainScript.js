fb9cc4e64dd892a725eb0225f52417c7270a0001
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-10-16 16:29:31 +0100

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -117,3 +117,24 @@ function updateLesson(txt){
 		}
 	}
 }
+
+const { Factory, EasyScore, System } = Vex.Flow;
+
+const vf = new Factory({
+  renderer: { elementId: 'lessonArea', width: 500, height: 200 },
+});
+
+const score = vf.EasyScore();
+const system = vf.System();
+
+system
+  .addStave({
+    voices: [
+      score.voice(score.notes('C#5/q, B4, A4, G#4', { stem: 'up' })),
+      score.voice(score.notes('C#4/h, C#4', { stem: 'down' })),
+    ],
+  })
+  .addClef('treble')
+  .addTimeSignature('4/4');
+
+vf.draw();
