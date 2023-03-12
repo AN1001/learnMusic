f05d2ba56fe65b8c7f13a59138f263d952196ddb
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2023-03-12 19:20:48 +0000

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -2,7 +2,7 @@
 //THIS CODE WAS CREATED BY ARNAV NAGPURE
 //**************************************
 //ADDITIONAL DEPENDANCIES INCLUDE VEX FLOW
-const LESSONS = ['G1L0','G1L1','G1L2','G1L3']
+const LESSONS = ['G1L0','G1L1','G1L2','G1L3','G1L4']
 var code;
 var xhttp = new XMLHttpRequest();
 var lessonInfo;
